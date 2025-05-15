04f24236b081a98d7935f77b4904077ffc149eb3
Mr Lii <leetamparega@gmail.com>
2025-05-15 12:43:38 +0000

    docs: add findByDateRange method to TaskDAO interface

--- src/main/java/com/novatech/dao/TaskDAO.java
+++ src/main/java/com/novatech/dao/TaskDAO.java
@@ -2,6 +2,7 @@ package com.novatech.dao;
 
 import com.novatech.model.Task;
 import java.sql.SQLException;
+import java.util.Date;
 import java.util.List;
 
 public interface TaskDAO {
@@ -9,6 +10,7 @@ public interface TaskDAO {
     List<Task> findAll() throws SQLException;
     List<Task> findByUserId(int userId) throws SQLException;
     List<Task> findByStatus(int userId, String status) throws SQLException;
+    List<Task> findByDateRange(int userId, Date startDate, Date endDate, String status) throws SQLException;
     int create(Task task) throws SQLException;
     boolean update(Task task) throws SQLException;
     boolean delete(int id) throws SQLException;
