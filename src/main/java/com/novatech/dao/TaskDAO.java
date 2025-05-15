36c3a57da41f2cc0427b1a298b603216730c1ab4
Mr Lii <leetamparega@gmail.com>
2025-05-15 13:19:23 +0000

    docs: parameter sortDecending boolean to findByDateRange to sort by specific date

--- src/main/java/com/novatech/dao/TaskDAO.java
+++ src/main/java/com/novatech/dao/TaskDAO.java
@@ -10,7 +10,7 @@ public interface TaskDAO {
     List<Task> findAll() throws SQLException;
     List<Task> findByUserId(int userId) throws SQLException;
     List<Task> findByStatus(int userId, String status) throws SQLException;
-    List<Task> findByDateRange(int userId, Date startDate, Date endDate, String status) throws SQLException;
+    List<Task> findByDateRange(int userId, Date startDate, Date endDate, String status, boolean sortDescending) throws SQLException;
     int create(Task task) throws SQLException;
     boolean update(Task task) throws SQLException;
     boolean delete(int id) throws SQLException;
