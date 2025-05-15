15956c1654c55fd317611e2f1732bdc55eeb77d3
Mr Lii <leetamparega@gmail.com>
2025-05-15 21:02:17 +0000

    docs: add commit to userDAO implememntation

--- src/main/java/com/novatech/dao/UserDAOImplementation.java
+++ src/main/java/com/novatech/dao/UserDAOImplementation.java
@@ -7,6 +7,9 @@ import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * USer Interface Implementation
+ */
 public class UserDAOImplementation implements UserDAO {
 
     @Override
